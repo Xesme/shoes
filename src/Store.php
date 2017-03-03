755e7c694eda87afdf4b0438dff50f9bfa904835
xa <xiaesmeamen@me.com>
2017-03-03 10:37:32 -0800

    remove store_id from tests/ and src/ pass test2 save()

--- src/Store.php
+++ src/Store.php
@@ -3,13 +3,11 @@
 class Store
 {
     private $store_name;
-    private $store_id;
     private $id;
 
-    function __construct($store_name = '', $store_id = null, $id = null)
+    function __construct($store_name = '', $id = NULL)
     {
         $this->store_name = $store_name;
-        $this->store_id = $store_id;
         $this->id = $id;
     }
 
@@ -20,9 +18,9 @@ class Store
         return $this->store_name;
     }
 
-    function setName($store_name)
+    function setName($new_store_name)
     {
-        $this->store_name = (string) $store_name;
+        $this->store_name = (string) $new_store_name;
     }
 
     function getId()
@@ -30,9 +28,34 @@ class Store
         $this->id;
     }
 
-    function getStoreId()
+    // function getStoreId()
+    // {
+    //     $this->store_id;
+    // }
+
+    // functions
+
+    function save()
     {
-        $this->store_id;
+        $GLOBALS['DB']->exec("INSERT INTO stores (store_name) VALUES ('{$this->getName()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+
+    //  static functions
+
+    static function getAll()
+    {
+        $returned_store = $GLOBALS['DB']->query("SELECT * FROM stores;");
+        $stores = array();
+        foreach($returned_store as $store)
+        {
+            $store_name = $store['store_name'];
+            $id = $store['id'];
+            $new_store = new Store($store_name, $id);
+            array_push($stores, $new_store);
+        }
+        return $stores;
     }
 }
 
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -1,16 +1,16 @@
 <?php
 
 /**
-* @backupGLOBALS disabled
+* @backupGlobals disabled
 * @backupStaticAttributes disabled
 **/
 
 require_once "src/Store.php";
 // require_once "src/Brand.php";
 
-$server= "mysql:host=localhost:8889;dbname=shoes_test";
-$username = 'root';
-$password = 'root';
+$server = "mysql:host=localhost:8889;dbname=shoes_test";
+$username = "root";
+$password = "root";
 $DB = new PDO($server, $username, $password);
 
 class StoreTest extends PHPUnit_Framework_TestCase
@@ -20,20 +20,32 @@ class StoreTest extends PHPUnit_Framework_TestCase
     {
         // Arrange
         $store_name = "PayMore";
-        $store_id = NULL;
         $id = NULL;
-        $new_store = new Store($store_name, $store_id, $id);
+        $new_store = new Store($store_name, $id);
 
         // Act
         $result1 = $new_store->getName();
-        $result2 = $new_store->getStoreId();
         $result3 = $new_store->getId();
 
         // Assert
         $this->assertEquals($store_name, $result1);
-        $this->assertEquals($store_id, $result2);
         $this->assertEquals($id, $result3);
 
+    }
+
+    function test_save_dependant_on_getAll()
+    {
+        // Arrange
+        $store_name = 'PayMore';
+        $id = NULL;
+        $new_test_store = new Store($store_name, $id);
+
+        // Act
+        $new_test_store->save();
+        $result = Store::getAll();
+
+        // Assert
+        $this->assertEquals([$new_test_store], $result);
     }
 
 }
