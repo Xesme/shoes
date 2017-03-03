154dd71b3d2bc6c8c2ad8dc4a06725edee9179fe
xa <xiaesmeamen@me.com>
2017-03-03 09:28:23 -0800

    add initial code for Store.php class Store (__construct, getters and setters)

---
+++ src/Store.php
@@ -0,0 +1,39 @@
+<?php
+
+class Store
+{
+    private $store_name;
+    private $store_id;
+    private $id;
+
+    function __construct($store_name = '', $store_id = null, $id = null)
+    {
+        $this->store_name;
+        $this->store_id;
+        $this->id:
+    }
+
+    // getters and setters
+
+    function getName()
+    {
+        return $this->store_name;
+    }
+
+    function setName($store_name)
+    {
+        $this->store_name = (string) $store_name;
+    }
+
+    function getId()
+    {
+        $this->id;
+    }
+
+    function getStoreId()
+    {
+        $this->store_id;
+    }
+}
+
+ ?>
